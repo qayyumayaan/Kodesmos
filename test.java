4c799d7e1d0d926b831cbe3a80513ae4b299d1b3
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2022-12-03 14:49:20 -0500

    Working

--- test.java
+++ test.java
@@ -1,38 +1,39 @@
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+// import java.awt.Color;
+// import java.awt.image.BufferedImage;
+// import java.io.File;
+// import java.io.IOException;
+// import javax.imageio.ImageIO;
 
 public class test {
 
-    public static int[][] loadPixelsFromFile(File file) throws IOException {
+    // public static int[][] loadPixelsFromFile(File file) throws IOException {
 
-        BufferedImage image = ImageIO.read(file);
-        int[][] grayscale = new int[image.getWidth()][image.getHeight()];
+    // BufferedImage image = ImageIO.read(file);
+    // int[][] grayscale = new int[image.getWidth()][image.getHeight()];
 
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
-                int color = image.getRGB(x, y);
-                int blue = color & 0xff;
-                int green = (color & 0xff00) >> 8;
-                int red = (color & 0xff000) >> 16;
-                grayscale[x][y] = (int) Math.round(.2126 * red + .7152 * green + .0722 * blue);
-            }
-        }
+    // for (int x = 0; x < image.getWidth(); x++) {
+    // for (int y = 0; y < image.getHeight(); y++) {
+    // int color = image.getRGB(x, y);
+    // int blue = color & 0xff;
+    // int green = (color & 0xff00) >> 8;
+    // int red = (color & 0xff000) >> 16;
+    // grayscale[x][y] = (int) Math.round(.2126 * red + .7152 * green + .0722 *
+    // blue);
+    // }
+    // }
 
-        return grayscale;
-    }
+    // return grayscale;
+    // }
 
-    public static void main(String[] args) throws IOException {
-        String pathname = args[0];
-        int[][] colors = loadPixelsFromFile(new File(pathname));
-        for (int i = 0; i < colors.length; i++) {
-            for (int j = 0; j < colors[i].length; j++) {
-                System.out.print(colors[i][j]);
-                if ((i + 1) * (j + 1) < colors.length * colors[i].length) 
-                    System.out.print(", ");
-            }
-        }
-    }
+    // public static void main(String[] args) throws IOException {
+    // String pathname = args[0];
+    // int[][] colors = loadPixelsFromFile(new File(pathname));
+    // for (int i = 0; i < colors.length; i++) {
+    // for (int j = 0; j < colors[i].length; j++) {
+    // System.out.print(colors[i][j]);
+    // if ((i + 1) * (j + 1) < colors.length * colors[i].length)
+    // System.out.print(", ");
+    // }
+    // }
+    // }
 }
