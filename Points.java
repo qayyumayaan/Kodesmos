9fd2686ec0ffb252720ab51addc0d0f8b8262597
ahmedali6395 <ahmedali6395@gmail.com>
2022-12-03 14:03:06 -0500

    boiboiboiboiboibobiboi

---
+++ Points.java
@@ -0,0 +1,19 @@
+class Points{
+    public static void main(String args[]){
+        /*for(double i = 1; i>=0; i-=0.1){
+            for(double j = 1; j>=0; j-=0.1){
+                System.out.printf("%.2f",i);
+                System.out.printf(", %.2f",j);    
+                System.out.println(); 
+
+            }   
+        }*/
+
+        for(int i = 0; i<=20; i++){
+            for(int j = 0; j<=20; j++){
+                System.out.println(i+", "+j);
+            }
+        }
+
+    }
+}
